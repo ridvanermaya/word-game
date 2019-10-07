3b6eb87f73b09e7ebf5c5bd6ebf2ac755064ec54
Ridvan Ermaya <ridvanermaya@gmail.com>
2019-10-07 10:46:08 -0400

    Created card for the general layout, added some properties to data

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <div id="app" class="container">
-    Hi There!
+    <div class="card mt-4">
+      <div class="card-header">
+        Question
+      </div>
+      <div class="card-body">
+        <div class="harf shadow">
+          <span>X</span>
+        </div>
+      </div>
+      <div class="card-footer">
+        Functions
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,9 +21,27 @@ export default {
   name: 'app',
   components: {
     
+  },
+  data() {
+    return {
+      questions: [],
+      question: "",
+      answer: "",
+      letters: [],
+      score: 0,
+      letterScore: 0
+    }
   }
 }
 </script>
 
 <style>
+.harf {
+  width: 100px;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 30pt;
+}
 </style>
