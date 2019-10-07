aa107be958fab26ae02b26d5acae7cdfddd58961
Ridvan Ermaya <ridvanermaya@gmail.com>
2019-10-07 12:09:03 -0400

    Time is added, finish function added

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
-  <div id="app" class="container">
-    <div class="card mt-4" v-if="!currentQuestion">
+  <div id="app" class="container mt-4">
+    <div class="card mb-4" v-if="completed">
+      <div class="card-body">
+        Congratulations you completed the game with the score of {{ this.score }}
+      </div>
+    </div>
+    <div class="card" v-if="!currentQuestion">
       <div class="card-body">
         To start the game please click "Start Game" button.
       </div>
       <div class="card-footer">
         <button class="btn btn-primary" @click="startGame">Start Game</button>
       </div>
+      
     </div>
-    <div class="card mt-4" v-else>
+    <div class="card" v-else>
       <div class="card-header">{{ currentQuestion.question }}</div>
       <div class="card-body">
         <div class="d-flex">
@@ -22,12 +28,15 @@
       <div class="card-footer">Total Score: {{ score }}</div>
       <div class="card-footer">
         <p>
-          <input type="text" class="form-control" placeholder="Type your answer" v-model="playerAnswer">
+          Remaining Time: <kbd>{{ this.remainingTime }}</kbd> seconds
+        </p>
+        <p>
+          <input type="text" class="form-control" placeholder="Type your answer" v-model="playerAnswer" @keyup="playerAnswer = playerAnswer.toUpperCase()">
         </p>
         <p>
           Your Answer: {{ playerAnswer}}
         </p>
-        <button @click="answer">Answer</button>
+        <button class="btn btn-success" @click="answer">Answer</button>
       </div>
       <div class="card-footer">
         <button class="btn btn-secondary" @click="giveLetter">Give Hint</button>
@@ -70,12 +79,37 @@ export default {
       letters: [],
       score: 0,
       letterScore: 0,
-      playerAnswer: ""
+      playerAnswer: "",
+      completed: false,
+      time: null,
+      remainingTime: 0
     }
   },
   methods: {
     startGame() {
+      this.currentQuestion = null;
+      this.completed = false;
+      this.score = 0;
+      this.questions.map(x => x.asked = false);
+      this.remainingTime = 240;
+      this.time = setInterval(() => {
+        this.remainingTime--;
+        
+        if (this.remainingTime === 0) {
+          this.finish();
+        }
+      }, 1000);
+
+      this.giveQuestion();
+    },
+    giveQuestion() {
+      this.playerAnswer = "";
       this.currentQuestion = this.questions.find(x => !x.asked);
+
+      if(!this.currentQuestion) {
+        this.finish();
+      }
+
       this.letters = [];
       this.currentQuestion.answer.split("").map(x => {
         this.letters.push({
@@ -84,6 +118,7 @@ export default {
         });
       });
       this.letterScore = this.letters.length * 100;
+      this.currentQuestion.asked = true;
     },
     giveLetter() {
       let randomLetterIndex = Math.floor(Math.random() * this.letters.length);
@@ -103,7 +138,21 @@ export default {
       this.letterScore -= 100;
     },
     answer() {
-      alert(this.playerAnswer);
+      let answer = this.playerAnswer.toUpperCase();
+      this.playerAnswer = answer;
+
+      if (this.playerAnswer === this.currentQuestion.answer.toUpperCase()) {
+        this.score += this.letterScore;
+      } else {
+        this.score -= this.letterScore;
+      }
+
+      this.giveQuestion();
+    },
+    finish() {
+      clearInterval(this.time);
+      this.currentQuestion = null;
+      this.completed = true;
     }
   }
 }
